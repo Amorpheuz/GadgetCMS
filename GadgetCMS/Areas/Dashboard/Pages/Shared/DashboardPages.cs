f479fab1c1ae82f6b21ee21bdc7bbea6e8e554ba
Yash Dave <mail2ypd@gmail.com>
2019-02-13 18:48:16 +0530

    Added ManageUsers under Dashboard and removed excess code in _ManageUser(Identity) nav

---
+++ GadgetCMS/Areas/Dashboard/Pages/Shared/DashboardPages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GadgetCMS.Areas.Dashboard.Pages.Shared
+{
+    public class DashboardPages
+    {
+        public static string Index => "Index";
+
+        public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+
+        private static string PageNavClass(ViewContext viewContext, string page)
+        {
+            var activePage = viewContext.ViewData["ActivePage"] as string
+                             ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
+            return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
+        }
+    }
+}
