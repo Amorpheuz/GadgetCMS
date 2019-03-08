ffd106ebde352f25cd05287f75fd3b23d5f38286
Yash Dave <mail2ypd@gmail.com>
2019-03-08 17:46:44 +0530

    Deafult Article Tables

---
+++ GadgetCMS/Pages/Article/Create.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using GadgetCMS.Data;
+
+namespace GadgetCMS.Pages.Article
+{
+    public class CreateModel : PageModel
+    {
+        private readonly GadgetCMS.Data.ApplicationDbContext _context;
+
+        public CreateModel(GadgetCMS.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Data.Article Article { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Article.Add(Article);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
