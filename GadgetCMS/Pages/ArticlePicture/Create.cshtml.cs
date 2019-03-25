92af2d303e8da7fcf640078822874c5b96617605
Yash Dave <mail2ypd@gmail.com>
2019-03-25 16:35:56 +0530

    Added Upload Image functionality

--- GadgetCMS/Pages/ArticlePicture/Create.cshtml.cs
+++ GadgetCMS/Pages/ArticlePicture/Create.cshtml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using GadgetCMS.Data;
+using Microsoft.AspNetCore.Http;
 
 namespace GadgetCMS.Pages.ArticlePicture
 {
@@ -20,23 +22,35 @@ namespace GadgetCMS.Pages.ArticlePicture
 
         public IActionResult OnGet()
         {
-        ViewData["ArticleId"] = new SelectList(_context.Article, "ArticleId", "ArticleAuthor");
+        ViewData["ArticleId"] = new SelectList(_context.Article, "ArticleId", "ArticleName");
             return Page();
         }
 
         [BindProperty]
         public Data.ArticlePicture ArticlePicture { get; set; }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(List<IFormFile> upFiles)
         {
-            if (!ModelState.IsValid)
+            foreach (var upFile in upFiles)
             {
-                return Page();
+                if (upFile != null || upFile.ContentType.ToLower().StartsWith("image/"))
+                {
+                    MemoryStream ms = new MemoryStream();
+                    upFile.OpenReadStream().CopyTo(ms);
+
+                    System.Drawing.Image image = System.Drawing.Image.FromStream(ms);
+
+                    Data.ArticlePicture upArticlePicture= new Data.ArticlePicture()
+                    {
+                        ArticleId = ArticlePicture.ArticleId,
+                        ArticlePictureCaption = ArticlePicture.ArticlePictureCaption,
+                        ArticlePictureBytes = ms.ToArray()
+                    };
+
+                    _context.ArticlePicture.Add(upArticlePicture);
+                    await _context.SaveChangesAsync();
+                }
             }
-
-            _context.ArticlePicture.Add(ArticlePicture);
-            await _context.SaveChangesAsync();
-
             return RedirectToPage("./Index");
         }
     }
