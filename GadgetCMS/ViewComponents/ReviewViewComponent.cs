51e4bdff68968030457cc4d4872d6cba35ab5089
Yash Dave <mail2ypd@gmail.com>
2019-03-27 20:07:32 +0530

    Integrated Review ViewComponent For bringing Reviews to Articles

---
+++ GadgetCMS/ViewComponents/ReviewViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GadgetCMS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GadgetCMS.ViewComponents
+{
+    public class ReviewViewComponent : ViewComponent
+    {
+        private readonly GadgetCMSContext _context;
+        public ReviewViewComponent(GadgetCMSContext context)
+        {
+            _context = context;
+        }
+
+        public List<Data.Review> Reviews { get; set; }
+
+        public async Task<IViewComponentResult> InvokeAsync(int ArticleId)
+        {
+            Reviews = await _context.Review
+                .Include(r => r.Article)
+                .Include(r => r.GadgetCmsUser).Where(r => r.ArticleId == ArticleId).ToListAsync();
+            
+            return View(Reviews);
+        }
+    }
+}
