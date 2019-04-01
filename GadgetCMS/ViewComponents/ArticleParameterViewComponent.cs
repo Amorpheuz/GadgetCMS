0e963e6bca90f01c5851082cd91c650c63a1e288
Yash Dave <mail2ypd@gmail.com>
2019-04-01 15:59:09 +0530

    Integrated ArticleParameter ViewComponent for bringing specs to Articles

---
+++ GadgetCMS/ViewComponents/ArticleParameterViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GadgetCMS.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GadgetCMS.ViewComponents
+{
+    public class ArticleParameterViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArticleParameterViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ArticleParameter> ArticleParameters { get; set; }
+
+        public async Task<IViewComponentResult> InvokeAsync(int articleId)
+        {
+            ArticleParameters = await _context.ArticleParameter
+                .Include(r => r.Parameter)
+                .ThenInclude(r => r.ParentParameter).Where(r => r.ArticleId == articleId).ToListAsync();
+            return View(ArticleParameters);
+        }
+    }
+}
