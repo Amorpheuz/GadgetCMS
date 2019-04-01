09259afdebf2d2861f9e2443799ff6abebedf117
Yash Dave <mail2ypd@gmail.com>
2019-04-01 17:25:25 +0530

    Integrated ArticlePictures ViewComponent for bringing pics to Articles

---
+++ GadgetCMS/ViewComponents/ArticlePictureViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GadgetCMS.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GadgetCMS.ViewComponents
+{
+    public class ArticlePictureViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+        public ArticlePictureViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ArticlePicture> ArticlePictures { get; set; }
+
+        public async Task<IViewComponentResult> InvokeAsync(int articleId)
+        {
+            ArticlePictures = await _context.ArticlePicture
+                .Include(a => a.Article).Where(a => a.ArticleId == articleId).ToListAsync();
+
+            return View(ArticlePictures);
+        }
+    }
+}
