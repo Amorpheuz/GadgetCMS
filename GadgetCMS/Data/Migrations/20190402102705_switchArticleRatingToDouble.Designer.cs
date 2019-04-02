ad299d697bfaca836dd8f2a964c6efb3a819a151
Yash Dave <mail2ypd@gmail.com>
2019-04-02 15:57:50 +0530

    switched Rating datatype to double (added migrations)

--- GadgetCMS/Data/Migrations/20190402102705_switchArticleRatingToDouble.Designer.cs
+++ GadgetCMS/Data/Migrations/20190402102705_switchArticleRatingToDouble.Designer.cs
@@ -4,14 +4,16 @@ using GadgetCMS.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GadgetCMS.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190402102705_switchArticleRatingToDouble")]
+    partial class switchArticleRatingToDouble
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GadgetCMS/Data/Migrations/20190402102705_switchArticleRatingToDouble.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GadgetCMS.Data.Migrations
+{
+    public partial class switchArticleRatingToDouble : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "ArticleRating",
+                table: "Article",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(int),
+                oldDefaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ArticleRating",
+                table: "Article",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(double),
+                oldDefaultValue: 0.0);
+        }
+    }
+}
