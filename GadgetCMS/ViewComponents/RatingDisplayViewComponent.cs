fbe3b96e9033f2c3771b7018cf24153af067f29b
Yash Dave <mail2ypd@gmail.com>
2019-04-02 16:17:38 +0530

    Configured Bar rating viewcomponent for display on article

---
+++ GadgetCMS/ViewComponents/RatingDisplayViewComponent.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GadgetCMS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GadgetCMS.ViewComponents
+{
+    public class RingControlModel
+    {
+        public List<SelectListItem> SelectedListItems { get; set; }
+        public string RatingControlValue { get; set; }
+        public string RatingControlIdValue { get; internal set; }
+    }
+
+    public class RatingDisplayViewComponent : ViewComponent
+    {
+        private readonly GadgetCMSContext _context;
+
+        public RatingDisplayViewComponent(GadgetCMSContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke(int articleId)
+        {
+            var ratingControlValues = new List<string>();
+            var ratingControlInitialValue = "";
+
+            ratingControlValues.Add("");
+            ratingControlValues.Add("1");
+            ratingControlValues.Add("2");
+            ratingControlValues.Add("3");
+            ratingControlValues.Add("4");
+            ratingControlValues.Add("5");
+
+            ratingControlInitialValue = _context.Article.Where(r => r.ArticleId == articleId).Select(r => r.ArticleRating).First().ToString();
+
+            List<SelectListItem> ratings = ratingControlValues.Select(
+                myValue => new SelectListItem
+                {
+                    Value = myValue,
+                    Text = myValue
+                }).ToList();
+
+            RingControlModel ratingControlModel = new RingControlModel
+            {
+                SelectedListItems = ratings,
+                RatingControlValue = ratingControlInitialValue,
+                RatingControlIdValue = "rating"+articleId
+            };
+
+            return View(ratingControlModel);
+        }
+    }
+}
