328a9ab7ca31cc36cbcf65379c85d7504f632ee5
Yash Dave <mail2ypd@gmail.com>
2019-04-10 21:38:09 +0530

    ViewComponents for displaying Company names

---
+++ GadgetCMS/ViewComponents/CompanyNamesViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using GadgetCMS.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GadgetCMS.ViewComponents
+{
+    public class CompanyNamesViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CompanyNamesViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<string> CompanyNames { get; set; }
+
+        public IViewComponentResult Invoke()
+        {
+            CompanyNames = _context.ArticleParameter
+                .Where(a => a.ParameterId == 5).Select(a => a.ParameterVal).Distinct().ToList();
+
+            return View(CompanyNames);
+        }
+    }
+}
