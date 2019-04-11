09ceedd78065fe321cb487ed0f0d2510000ee59a
Unknown <alkesh302@gmail.com>
2019-04-11 18:11:33 +0530

    Migration Files of adding Featured Column in Article Model

---
+++ GadgetCMS/Data/Migrations/20190411113427_ArticleFeatured_Article.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GadgetCMS.Data.Migrations
+{
+    public partial class ArticleFeatured_Article : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ArticlePictureBytes",
+                table: "ArticlePicture",
+                nullable: true,
+                oldClrType: typeof(byte[]));
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "ArticleVisible",
+                table: "Article",
+                nullable: true,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldDefaultValue: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "ArticleEditLock",
+                table: "Article",
+                nullable: true,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldDefaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Featured",
+                table: "Article",
+                nullable: true,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Featured",
+                table: "Article");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ArticlePictureBytes",
+                table: "ArticlePicture",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "ArticleVisible",
+                table: "Article",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldNullable: true,
+                oldDefaultValue: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "ArticleEditLock",
+                table: "Article",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldNullable: true,
+                oldDefaultValue: false);
+        }
+    }
+}
