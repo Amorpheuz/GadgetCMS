456e36ad100fd76a294863e1bb69abb0ff8113d4
Unknown <alkesh302@gmail.com>
2019-04-11 11:59:33 +0530

    CreatedAdvancedSearchModel
    First Custom Model of Advanced Search page
    Total Custom Models are : 2 (AS and FA)

---
+++ GadgetCMS/Data/AdvancedSearch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GadgetCMS.Data
+{
+    public class AdvancedSearch
+    {
+        public int AdvancedSearchId {get;set;}
+
+        public List<Data.Article> Articles{get;set;}
+        public List<Data.Parameter> Parameters {get;set;}
+       
+        public List<Data.ArticleParameter> ArticleParameters { get; set; }
+    }
+}
