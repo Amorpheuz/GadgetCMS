802a0185449d639c0e4cd7afb07beb00445d9427
Unknown <alkesh302@gmail.com>
2019-04-11 12:04:51 +0530

    Created FilteredArticle Model
    Second Custom Model for Advanced Search Page, Called after Filters are applied

---
+++ GadgetCMS/Data/FilteredArticle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GadgetCMS.Data
+{
+    public class FilteredArticle
+    {
+        public int FilteredArticleId {get;set;}
+        public string ParameterValues {get;set;}
+        public List<Data.Article> Articles {get;set;}
+        private GadgetCMS.Data.ApplicationDbContext ApplicationDbContext{get;}
+
+        public FilteredArticle(GadgetCMS.Data.ApplicationDbContext applicationDbContext)
+        {
+            ApplicationDbContext = applicationDbContext;
+        }
+        
+        public FilteredArticle()
+        {
+            
+        }
+
+        public List<Data.ArticleParameter> getValues(string abc)
+        {
+            return ApplicationDbContext.ArticleParameter
+                .Where(b => b.ParameterVal == abc).ToList();
+        }
+    }
+}
