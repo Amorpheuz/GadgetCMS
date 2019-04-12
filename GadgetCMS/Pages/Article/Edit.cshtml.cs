b0c88d973d1bf934666725d180b2b9603f4f885a
Yash Dave <mail2ypd@gmail.com>
2019-04-12 16:50:02 +0530

    Configured Article and Article Parameters Edit

--- GadgetCMS/Pages/Article/Edit.cshtml.cs
+++ GadgetCMS/Pages/Article/Edit.cshtml.cs
@@ -22,6 +22,11 @@ namespace GadgetCMS.Pages.Article
         [BindProperty]
         public Data.Article Article { get; set; }
 
+        [BindProperty]
+        public List<Data.ArticleParameter> ArticleParameters { get; set; }
+
+        public List<IGrouping<int, Data.ArticleParameter>> ListParameters { get; set; }
+
         public async Task<IActionResult> OnGetAsync(int? id)
         {
             if (id == null)
@@ -30,13 +35,24 @@ namespace GadgetCMS.Pages.Article
             }
 
             Article = await _context.Article
-                .Include(a => a.Category).FirstOrDefaultAsync(m => m.ArticleId == id);
+                .Include(a => a.Category)
+                .Include(a => a.ArticleParameters)
+                .Include(a => a.ArticlePictures)
+                .FirstOrDefaultAsync(m => m.ArticleId == id);
 
+            ListParameters = await _context.ArticleParameter
+                .Include(a => a.Article)
+                .Include(a => a.Parameter)
+                    .ThenInclude(a => a.ParentParameter)
+                .Where(a => a.ArticleId == id).GroupBy(a => a.Parameter.ParentParameter.ParentParameterId).ToListAsync();
+
+            ArticleParameters = ListParameters.SelectMany(a => a).ToList();
+            
             if (Article == null)
             {
                 return NotFound();
             }
-           ViewData["CategoryId"] = new SelectList(_context.Set<Data.Category>(), "CategoryId", "CategoryDescription");
+            ViewData["CategoryId"] = new SelectList(_context.Set<Data.Category>(), "CategoryId", "CategoryName");
             return Page();
         }
 
@@ -44,10 +60,13 @@ namespace GadgetCMS.Pages.Article
         {
             if (!ModelState.IsValid)
             {
+                ViewData["CategoryId"] = new SelectList(_context.Set<Data.Category>(), "CategoryId", "CategoryName");
+                CheckArticleContent();
                 return Page();
             }
 
-            _context.Attach(Article).State = EntityState.Modified;
+            _context.Update(Article);
+            _context.UpdateRange(ArticleParameters);
 
             try
             {
@@ -71,6 +90,14 @@ namespace GadgetCMS.Pages.Article
         private bool ArticleExists(int id)
         {
             return _context.Article.Any(e => e.ArticleId == id);
+        }
+
+        private void CheckArticleContent()
+        {
+            if (Article.ArticleContent != null || Article.ArticleContent != "")
+            {
+                ViewData["ArticleContent"] = Article.ArticleContent;
+            }
         }
     }
 }
