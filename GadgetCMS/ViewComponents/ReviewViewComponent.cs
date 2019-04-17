0972bb65f2253771cfecedf359cb57da1048ba2d
Yash Dave <mail2ypd@gmail.com>
2019-04-17 18:32:21 +0530

    Added check for whether user has comment on article

--- GadgetCMS/ViewComponents/ReviewViewComponent.cs
+++ GadgetCMS/ViewComponents/ReviewViewComponent.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using GadgetCMS.Areas.Identity.Data;
 using GadgetCMS.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,19 +14,29 @@ namespace GadgetCMS.ViewComponents
     public class ReviewViewComponent : ViewComponent
     {
         private readonly GadgetCMSContext _context;
-        public ReviewViewComponent(GadgetCMSContext context)
+        private readonly UserManager<GadgetCMSUser> _userManager;
+        public ReviewViewComponent(UserManager<GadgetCMSUser> userManager, GadgetCMSContext context)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public List<Data.Review> Reviews { get; set; }
-
+                
         public async Task<IViewComponentResult> InvokeAsync(int ArticleId)
         {
             Reviews = await _context.Review
                 .Include(r => r.Article)
                 .Include(r => r.GadgetCmsUser).Where(r => r.ArticleId == ArticleId).ToListAsync();
-            
+
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            foreach (var item in Reviews)
+            {
+                if(item.GadgetCmsUser.Email == user.Email)
+                {
+                    ViewData["UserHasComment"] = true;
+                }
+            }            
             return View(Reviews);
         }
     }
