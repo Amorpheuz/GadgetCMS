3eda7b36824ed9ab8e1edeed60712e23dbcc0f75
Yash Dave <mail2ypd@gmail.com>
2019-05-07 13:27:09 +0530

    Added Log Downloads Page

--- GadgetCMS/Areas/Dashboard/Pages/DownloadNLogFiles.cshtml.cs
+++ GadgetCMS/Areas/Dashboard/Pages/DownloadNLogFiles.cshtml.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -9,18 +12,36 @@ namespace GadgetCMS.Areas.Dashboard.Pages
 {
     public class DownloadNLogFilesModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            return Page();
         }
 
-        //public IActionResult OnPost()
-        //{
-        //    var net = new System.Net.WebClient();
-        //    var data = net.DownloadData("Nlog");
-        //    var content = new System.IO.MemoryStream(data);
-        //    var contentType = "APPLICATION/octet-stream";
-        //    var fileName = "nlog-own.txt";
-        //    return File(content, contentType, fileName);
-        //}
+        public async Task<IActionResult> OnPostAsync()
+        {
+            string startPath = @".\Nlog";
+            string zipPath = @".\LogZip\Logs.zip";
+            var filename = "Logs.zip";
+
+            if (System.IO.File.Exists(Path.Combine(Directory.GetCurrentDirectory(),"LogZip", filename)))
+            {
+                System.IO.File.Delete(Path.Combine(Directory.GetCurrentDirectory(), "LogZip", filename));
+            }
+
+            ZipFile.CreateFromDirectory(startPath,zipPath);
+
+            
+            var path = Path.Combine(
+                           Directory.GetCurrentDirectory(),
+                           "LogZip", filename);
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, "application/zip", Path.GetFileName(path));
+        }
     }
 }
